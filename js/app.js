e6be8ca10ddceea985c7ca30960792808d7716f5
Corey Cunningham <coreyjjc@me.com>
2017-09-01 14:59:54 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$(document).foundation();
+$(document).ready(function(){
+
+$(".slider").slick({dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear',
+  mobileFirst: true
+  });
+
+});
